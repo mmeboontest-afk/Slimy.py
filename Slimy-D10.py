af241329ca88da51a5e79aae8695ed363309079b
mmeboontest-afk <mmeboontest@gmail.com>
2026-01-13 20:17:34 +0700

    Update Slimy-D10.py

--- Slimy-D10.py
+++ Slimy-D10.py
@@ -3,28 +3,28 @@ import os
 from flask import Flask
 from threading import Thread
 
-# ส่วนหลอกระบบ Koyeb (Health Check)
+# ส่วนสร้างหน้าเว็บหลอกเพื่อผ่าน Health Check ของ Koyeb
 app = Flask('')
 @app.route('/')
 def home():
-    return "I'm alive"
+    return "Bot is running!"
 
 def run():
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=8080)
 
 def keep_alive():
     t = Thread(target=run)
     t.start()
 
-# ส่วนของบอท Discord
+# ส่วนการทำงานของบอท Discord
 intents = discord.Intents.default()
 intents.message_content = True 
 client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print(f'Logged in as {client.user}')
+    print(f'บอทออนไลน์แล้วในชื่อ: {client.user}')
 
-keep_alive() # เริ่มรันเซิร์ฟเวอร์หลอก
-token = os.getenv('BOT_TOKEN')
+keep_alive() # สั่งรันเว็บหลอก
+token = os.getenv('BOT_TOKEN') # ดึง Token จากค่าที่เราจะตั้งใน Koyeb
 client.run(token)
